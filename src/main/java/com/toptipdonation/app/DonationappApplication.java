ed9055dddcac763d6a10036f1281c1c1d5e5b2de
fabian <fabian.polkosnik@gmail.com>
2018-03-12 22:46:23 +0100

    init

---
+++ src/main/java/com/toptipdonation/app/DonationappApplication.java
@@ -0,0 +1,12 @@
+package com.toptipdonation.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationappApplication.class, args);
+	}
+}
